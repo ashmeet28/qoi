848c0fa22c4e720bf3d37e0dadd4d76da34552bc
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2024-05-18 01:32:12 -0400

    Changed to pkg

--- qoi.go
+++ qoi.go
@@ -1,4 +1,4 @@
-package main
+package qoi
 
 import "encoding/binary"
 
@@ -6,43 +6,43 @@ var ColorspaceSRGB uint8 = 0
 var ColorspaceLinear uint8 = 1
 
 var (
-	qoiOpIndex uint8 = 0b00000000
-	qoiOpDiff  uint8 = 0b01000000
-	qoiOpLuma  uint8 = 0b10000000
-	qoiOpRun   uint8 = 0b11000000
-	qoiOpRGB   uint8 = 0b11111110
-	qoiOpRGBA  uint8 = 0b11111111
+	opIndex uint8 = 0b00000000
+	opDiff  uint8 = 0b01000000
+	opLuma  uint8 = 0b10000000
+	opRun   uint8 = 0b11000000
+	opRGB   uint8 = 0b11111110
+	opRGBA  uint8 = 0b11111111
 )
 
-var qoiMagic []uint8 = []byte("qoif")
+var magic []uint8 = []byte("qoif")
 
-var qoiPadding []uint8 = []byte{0, 0, 0, 0, 0, 0, 0, 1}
+var padding []uint8 = []byte{0, 0, 0, 0, 0, 0, 0, 1}
 
-type qoiHeader struct {
+type Header struct {
 	width      uint32
 	height     uint32
 	channels   uint8
 	colorspace uint8
 }
 
-type qoiRGBA struct {
+type pxRGBA struct {
 	r uint8
 	g uint8
 	b uint8
 	a uint8
 }
 
-func qoiColorHash(c qoiRGBA) uint8 {
-	return (c.r*3 + c.g*5 + c.b*7 + c.a*11) % 64
+func pxHash(px pxRGBA) uint8 {
+	return (px.r*3 + px.g*5 + px.b*7 + px.a*11) % 64
 }
 
-func Encode(data []byte, desc qoiHeader) []byte {
+func Encode(data []byte, desc Header) []byte {
 	var pxRun uint8
 
-	var pxIndex []qoiRGBA = make([]qoiRGBA, 64)
+	var pxIndex []pxRGBA = make([]pxRGBA, 64)
 
-	var px qoiRGBA
-	var pxPrev qoiRGBA
+	var px pxRGBA
+	var pxPrev pxRGBA
 
 	var pxChannels uint8
 
@@ -52,7 +52,7 @@ func Encode(data []byte, desc qoiHeader) []byte {
 	var pxLen uint32
 	var pxEnd uint32
 
-	encData = append(encData, qoiMagic...)
+	encData = append(encData, magic...)
 	encData = binary.BigEndian.AppendUint32(encData, desc.width)
 	encData = binary.BigEndian.AppendUint32(encData, desc.height)
 	encData = append(encData, desc.channels)
@@ -90,19 +90,19 @@ func Encode(data []byte, desc qoiHeader) []byte {
 		if px == pxPrev {
 			pxRun += 1
 			if pxRun == 62 || pxPos == pxEnd {
-				encData = append(encData, qoiOpRun|pxRun)
+				encData = append(encData, opRun|pxRun)
 				pxRun = 0
 			}
 		} else {
 			if pxRun > 0 {
-				encData = append(encData, qoiOpRun|pxRun)
+				encData = append(encData, opRun|pxRun)
 				pxRun = 0
 			}
 
-			pxIndexPos := qoiColorHash(px)
+			pxIndexPos := pxHash(px)
 
 			if pxIndex[pxIndexPos] == px {
-				encData = append(encData, qoiOpIndex|pxIndexPos)
+				encData = append(encData, opIndex|pxIndexPos)
 			} else {
 				pxIndex[pxIndexPos] = px
 
@@ -117,23 +117,23 @@ func Encode(data []byte, desc qoiHeader) []byte {
 					if vr > 253 && vr < 2 &&
 						vg > 253 && vg < 2 &&
 						vb > 253 && vb < 2 {
-						encData = append(encData, qoiOpDiff|((vr+2)<<4)|((vg+2)<<2)|(vb+2))
+						encData = append(encData, opDiff|((vr+2)<<4)|((vg+2)<<2)|(vb+2))
 					} else if vgr > 247 && vgr < 8 &&
 						vg > 223 && vg < 32 &&
 						vgb > 247 && vgb < 8 {
-						encData = append(encData, qoiOpLuma|(vg+32), ((vgr+8)<<4)|(vgb+8))
+						encData = append(encData, opLuma|(vg+32), ((vgr+8)<<4)|(vgb+8))
 					} else {
-						encData = append(encData, qoiOpRGB, px.r, px.g, px.b)
+						encData = append(encData, opRGB, px.r, px.g, px.b)
 					}
 				} else {
-					encData = append(encData, qoiOpRGBA, px.r, px.g, px.b, px.a)
+					encData = append(encData, opRGBA, px.r, px.g, px.b, px.a)
 				}
 			}
 		}
 		pxPrev = px
 	}
 
-	encData = append(encData, qoiPadding...)
+	encData = append(encData, padding...)
 
 	return encData
 }
